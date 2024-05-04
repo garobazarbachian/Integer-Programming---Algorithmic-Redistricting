53ae2c3e2ca008d180fc104b4fe0b0dc1f76f520
garobazarbachian <gbazarbachian@gmail.com>
2024-05-04 12:13:17 -0600

    Part 5 push 1

---
+++ Part 5.py
@@ -0,0 +1,25 @@
+import geopandas as gpd
+import matplotlib.pyplot as plt
+
+# Load shapefile of Indiana counties
+indiana_counties = gpd.read_file("path/to/indiana_boundary_uscb_2020_tl_2020_18_state20.shp")
+
+# Load redistricting data
+redistricting_data = pd.read_csv("path/to/redistricting_data.csv")
+
+# Merge shapefile with redistricting data
+merged_data = indiana_counties.merge(redistricting_data, on="county_id")
+
+# Plot chloropleth map for algorithmic/optimal redistricting
+fig, ax = plt.subplots(1, 1)
+merged_data.plot(column="congressional_district", cmap="viridis", linewidth=0.5, ax=ax, edgecolor="0.8")
+ax.axis("off")
+ax.set_title("Algorithmic/Optimal Redistricting")
+plt.show()
+
+# Plot chloropleth map for actual redistricting
+fig, ax = plt.subplots(1, 1)
+merged_data.plot(column="actual_congressional_district", cmap="viridis", linewidth=0.5, ax=ax, edgecolor="0.8")
+ax.axis("off")
+ax.set_title("Actual Redistricting")
+plt.show()
